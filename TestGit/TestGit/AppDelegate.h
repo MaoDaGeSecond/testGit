a1c64e4837a8abef23b3387f7ff1c82499e467f7
Cheng Kai <chengkai@zkingsoft.com>
2015-03-24 10:56:05 +0800

    原始版本

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Chengkai on 15/3/24.
+//  Copyright (c) 2015年 Chengkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Chengkai on 15/3/24.
+//  Copyright (c) 2015年 Chengkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
